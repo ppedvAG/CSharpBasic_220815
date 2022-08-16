aa03ad6a39de606f8eb0c866ef0fa0b53832d918
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-08-16 14:20:46 +0200

    OOP

---
+++ Klassenbibliothek_Beispiel/Lebewesen.cs
@@ -0,0 +1,43 @@
+﻿namespace Klassenbibliothek_Beispiel
+{
+    public class Lebewesen
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length > 3)
+                    name = value;
+            }
+        }
+
+        public string Lieblingsnahrung { get; set; }
+
+        public DateTime Geburtsdatum { get; set; }
+
+        public int Alter { get { return (DateTime.Now - this.Geburtsdatum).Days / 365; } }
+
+        public int Größe { get; set; }
+
+        public Lebewesen(string name, string lieblingsnahrung, DateTime geburtstag, int größe)
+        {
+            this.name = name;
+            this.Lieblingsnahrung = lieblingsnahrung;
+            this.Geburtsdatum = geburtstag;
+            this.Größe = größe;
+        }
+
+        public void Wachse()
+        {
+            this.Größe++;
+        }
+
+        public Lebewesen GebäreKind(string kindname)
+        {
+            return new Lebewesen(kindname, "Muttermilch", DateTime.Now, 30);
+        }
+    }
+}
---
+++ TesteBibliothek/Program.cs
@@ -0,0 +1,31 @@
+﻿using Klassenbibliothek_Beispiel;
+
+namespace TesteBibliothek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Klassenbibliothek_Beispiel.Lebewesen lebewesen1;
+
+            lebewesen1 = new Lebewesen("Hugo Schmidt", "Pizza", new DateTime(2002, 2, 12), 185);
+            Lebewesen lebewesen2 = new Lebewesen("Bello", "Knochen", new DateTime(2020, 3, 15), 50);
+
+            Console.WriteLine("Lebewesen1 heißt " +lebewesen1.Name);
+            lebewesen1.Name = "Anna Schmidt";
+
+            Console.WriteLine("Lebewesen1 heißt " + lebewesen1.Name);
+            Console.WriteLine("Lebewesen2 heißt " + lebewesen2.Name);
+
+            Console.WriteLine(lebewesen2.Alter);
+
+            lebewesen1.Lieblingsnahrung = "Spagetti";
+
+            lebewesen1.Wachse();
+
+            lebewesen2.Wachse();
+
+            Lebewesen lebewesen3 = lebewesen1.GebäreKind("Maria");
+        }
+    }
+}
